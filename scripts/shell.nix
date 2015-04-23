50a1c86f5fb2f2751d67bbe66ae7966f48d539d3
Alp Mestanogullari <alpmestan@gmail.com>
2015-04-23 16:31:08 +0200

    add servant-examples to sources.txt and scripts/shell.nix

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -5,16 +5,18 @@ let modifiedHaskellPackages = haskellngPackages.override {
       overrides = with haskell-ng.lib ; self: super: {
         servant = appendConfigureFlag ( self.callPackage ../servant {} )
             "--ghc-options=-Werror";
-        servant-server = appendConfigureFlag (self.callPackage
+        servant-server   = appendConfigureFlag (self.callPackage
                 ../servant-server {}) "--ghc-options=-Werror";
-        servant-client = appendConfigureFlag (self.callPackage
+        servant-client   = appendConfigureFlag (self.callPackage
             ../servant-client {}) "--ghc-options=-Werror";
-        servant-jquery = appendConfigureFlag (self.callPackage
+        servant-jquery   = appendConfigureFlag (self.callPackage
             ../servant-jquery {}) "--ghc-options=-Werror";
-        servant-docs   = appendConfigureFlag (self.callPackage ../servant-docs
+        servant-docs     = appendConfigureFlag (self.callPackage ../servant-docs
+            {}) "--ghc-options=-Werror";
+        servant-examples = appendConfigureFlag (self.callPackage ../servant-examples
             {}) "--ghc-options=-Werror";
       };
     };
 in modifiedHaskellPackages.ghcWithPackages ( p : with p ; [
-    servant servant-server servant-client servant-jquery servant-docs
+    servant servant-server servant-client servant-jquery servant-docs servant-examples
 ])
