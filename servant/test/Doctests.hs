85b23772083adebe6a522ce3185f1aecf4422e45
Sönke Hahn <soenkehahn@gmail.com>
2015-07-14 01:07:19 +0800

    add stack.yaml and use it in travis file

--- servant/test/Doctests.hs
+++ servant/test/Doctests.hs
@@ -9,21 +9,8 @@ import           Test.DocTest
 main :: IO ()
 main = do
     files <- find always (extension ==? ".hs") "src"
-    cabalMacrosFile <- getCabalMacrosFile
     doctest $ [ "-isrc"
-              , "-optP-include"
-              , "-optP" ++ cabalMacrosFile
               , "-XOverloadedStrings"
               , "-XFlexibleInstances"
               , "-XMultiParamTypeClasses"
               ] ++ files
-
-getCabalMacrosFile :: IO FilePath
-getCabalMacrosFile = do
-  contents <- getDirectoryContents "dist"
-  let rest = "build" </> "autogen" </> "cabal_macros.h"
-  return $ case filter ("dist-sandbox-" `isPrefixOf`) contents of
-    [x] -> "dist" </> x </> rest
-    [] -> "dist" </> rest
-    xs -> error $ "ran doctests with multiple dist/dist-sandbox-xxxxx's: \n"
-                ++ show xs ++ "\nTry cabal clean"
