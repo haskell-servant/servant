e24cb9ddf677f43cba7a4a26dc163e7d306d34fe
Sönke Hahn <soenkehahn@gmail.com>
2015-07-25 17:22:05 +0800

    add stack file

--- servant-server/test/Doctests.hs
+++ servant-server/test/Doctests.hs
@@ -9,23 +9,33 @@ import           Test.DocTest
 main :: IO ()
 main = do
     files <- find always (extension ==? ".hs") "src"
-    cabalMacrosFile <- getCabalMacrosFile
-    doctest $ [ "-isrc"
-              , "-optP-include"
-              , "-optP" ++ cabalMacrosFile
-              , "-XOverloadedStrings"
-              , "-XFlexibleInstances"
-              , "-XMultiParamTypeClasses"
-              , "-XDataKinds"
-              , "-XTypeOperators"
-              ] ++ files
+    mCabalMacrosFile <- getCabalMacrosFile
+    doctest $ "-isrc" :
+              (maybe [] (\ f -> ["-optP-include", "-optP" ++ f]) mCabalMacrosFile) ++
+              "-XOverloadedStrings" :
+              "-XFlexibleInstances" :
+              "-XMultiParamTypeClasses" :
+              "-XDataKinds" :
+              "-XTypeOperators" :
+              files
 
-getCabalMacrosFile :: IO FilePath
+getCabalMacrosFile :: IO (Maybe FilePath)
 getCabalMacrosFile = do
-  contents <- getDirectoryContents "dist"
-  let rest = "build" </> "autogen" </> "cabal_macros.h"
-  return $ case filter ("dist-sandbox-" `isPrefixOf`) contents of
-    [x] -> "dist" </> x </> rest
-    [] -> "dist" </> rest
-    xs -> error $ "ran doctests with multiple dist/dist-sandbox-xxxxx's: \n"
-                ++ show xs ++ "\nTry cabal clean"
+  exists <- doesDirectoryExist "dist"
+  if exists
+    then do
+      contents <- getDirectoryContents "dist"
+      let rest = "build" </> "autogen" </> "cabal_macros.h"
+      whenExists $ case filter ("dist-sandbox-" `isPrefixOf`) contents of
+        [x] -> "dist" </> x </> rest
+        [] -> "dist" </> rest
+        xs -> error $ "ran doctests with multiple dist/dist-sandbox-xxxxx's: \n"
+                    ++ show xs ++ "\nTry cabal clean"
+    else return Nothing
+ where
+  whenExists :: FilePath -> IO (Maybe FilePath)
+  whenExists file = do
+    exists <- doesFileExist file
+    return $ if exists
+      then Just file
+      else Nothing
