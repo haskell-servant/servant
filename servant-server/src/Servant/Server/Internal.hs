db0931941b29849c025bd5447b8ceb6bff7dbb3e
aaron levin <vilevin@gmail.com>
2015-08-08 11:50:40 -0400

    Cleanup import failures and compiler warnings

--- servant-server/src/Servant/Server/Internal.hs
+++ servant-server/src/Servant/Server/Internal.hs
@@ -33,7 +33,7 @@ import qualified Data.ByteString.Lazy                       as BL
 import qualified Data.Map                                   as M
 import           Data.Maybe                  (mapMaybe, fromMaybe)
 import           Data.String                                (fromString)
-import           Data.String.Conversions                    (cs, (<>), ConvertibleStrings)
+import           Data.String.Conversions                    (ConvertibleStrings, cs, (<>))
 import           Data.Text                                  (Text)
 import qualified Data.Text                                  as T
 import           Data.Text.Encoding                         (decodeUtf8,
@@ -50,6 +50,8 @@ import           Network.Wai                                (Application,
                                                              lazyRequestBody,
                                                              rawQueryString,
                                                              remoteHost,
+                                                             Response,
+                                                             Request,
                                                              requestHeaders,
                                                              requestMethod,
                                                              responseLBS, vault)
@@ -79,8 +81,8 @@ import           Servant.Common.Text                        (FromText, fromText)
 import           Servant.Server.Internal.Authentication     (AuthData (authData),
                                                              AuthProtected (..),
                                                              checkAuthStrict,
-                                                             onMissingAuthData,
-                                                             onUnauthenticated)
+                                                             AuthHandlers(onMissingAuthData,
+                                                             onUnauthenticated))
 import           Servant.Server.Internal.PathInfo
 import           Servant.Server.Internal.Router
 import           Servant.Server.Internal.RoutingApplication
