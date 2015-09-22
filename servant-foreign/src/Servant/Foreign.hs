99b59eac396cff1fa73325a2b0211ee8c49373ef
Denis Redozubov <denis.redozubov@gmail.com>
2015-09-22 14:46:32 +0300

    NullaryTypeClasses needed for ghc 7.8

--- servant-foreign/src/Servant/Foreign.hs
+++ servant-foreign/src/Servant/Foreign.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NullaryTypeClasses    #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
