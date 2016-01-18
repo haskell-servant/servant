3fdaffa08dea2e2806c044b7b21d86823fe2a73b
Sönke Hahn <soenkehahn@gmail.com>
2016-01-18 12:29:33 +0100

    servant-mock: add simple test case

--- servant-mock/test/Servant/MockSpec.hs
+++ servant-mock/test/Servant/MockSpec.hs
@@ -1,12 +1,42 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Servant.MockSpec where
 
-import           Test.Hspec
+import           Data.Aeson as Aeson
+import           Data.Proxy
+import           GHC.Generics
+import           Servant.API
+import           Test.Hspec hiding (pending)
+import           Test.Hspec.Wai
+import           Test.QuickCheck
 
+import           Servant
 import           Servant.API.Internal.Test.ComprehensiveAPI
 import           Servant.Mock
 
 _ = mock comprehensiveAPI
 
+data Body
+  = Body
+  | ArbitraryBody
+  deriving (Generic, ToJSON)
+
+instance Arbitrary Body where
+  arbitrary = return ArbitraryBody
+
 spec :: Spec
-spec = return ()
+spec = do
+  describe "mock" $ do
+    context "Get" $ do
+      let api :: Proxy (Get '[JSON] Body)
+          api = Proxy
+          app = serve api (mock api)
+      with (return app) $ do
+        it "serves arbitrary response bodies" $ do
+          get "/" `shouldRespondWith` 200{
+            matchBody = Just $ Aeson.encode ArbitraryBody
+          }
