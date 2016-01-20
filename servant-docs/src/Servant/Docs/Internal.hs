a6de4ba1235943c1991d90105e8cdc28e01489d1
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-01-20 17:56:52 +0300

    Remove IsIn from servant-docs

--- servant-docs/src/Servant/Docs/Internal.hs
+++ servant-docs/src/Servant/Docs/Internal.hs
@@ -42,6 +42,7 @@ import           GHC.Generics
 import           GHC.TypeLits
 import           Servant.API
 import           Servant.API.ContentTypes
+import           Servant.API.TypeLevel
 import           Servant.Utils.Links
 
 import qualified Data.HashMap.Strict        as HM
@@ -305,15 +306,6 @@ docs p = docsWithOptions p defaultDocOptions
 -- | Generate the docs for a given API that implements 'HasDocs'.
 docsWithOptions :: HasDocs api => Proxy api -> DocOptions -> API
 docsWithOptions p = docsFor p (defEndpoint, defAction)
-
--- | Closed type family, check if endpoint is exactly within API.
-
--- We aren't sure what affects how an Endpoint is built up, so we require an
--- exact match.
-type family IsIn (endpoint :: *) (api :: *) :: Constraint where
-    IsIn e (sa :<|> sb)                = Or (IsIn e sa) (IsIn e sb)
-    IsIn (e :> sa) (e :> sb)           = IsIn sa sb
-    IsIn e e                           = ()
 
 -- | Create an 'ExtraInfo' that is guaranteed to be within the given API layout.
 --
