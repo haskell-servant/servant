4045d20c8d218ac5ab2f74780b3a1b9714aece64
Amar <amar@capital-match.com>
2016-04-21 20:30:11 +0800

    Fix unused warning

--- servant-client/src/Servant/Client.hs
+++ servant-client/src/Servant/Client.hs
@@ -43,7 +43,6 @@ import           Servant.Client.Experimental.Auth
 import           Servant.Common.BaseUrl
 import           Servant.Common.BasicAuth
 import           Servant.Common.Req
-import           Servant.Utils.Enter
 
 -- * Accessing APIs as a Client
 
