29be5761cecd0e88ce3b8b24a28a9a28dca0e135
Sönke Hahn <soenkehahn@gmail.com>
2016-04-21 15:27:08 +0800

    servant-client: add Eq instance for ServantError

--- servant-client/src/Servant/Common/Req.hs
+++ servant-client/src/Servant/Common/Req.hs
@@ -55,6 +55,19 @@ data ServantError
     }
   deriving (Show, Typeable)
 
+instance Eq ServantError where
+  FailureResponse a b c == FailureResponse x y z =
+    (a, b, c) == (x, y, z)
+  DecodeFailure a b c == DecodeFailure x y z =
+    (a, b, c) == (x, y, z)
+  UnsupportedContentType a b == UnsupportedContentType x y =
+    (a, b) == (x, y)
+  InvalidContentTypeHeader a b == InvalidContentTypeHeader x y =
+    (a, b) == (x, y)
+  ConnectionError a == ConnectionError x =
+    show a == show x
+  _ == _ = False
+
 instance Exception ServantError
 
 data Req = Req
