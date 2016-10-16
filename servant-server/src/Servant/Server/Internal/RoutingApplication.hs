6ab0296d62aac99af386280447dc7227da4fc9e3
Alp Mestanogullari <alpmestan@gmail.com>
2016-10-16 10:21:01 +0200

    fix a warning

--- servant-server/src/Servant/Server/Internal/RoutingApplication.hs
+++ servant-server/src/Servant/Server/Internal/RoutingApplication.hs
@@ -257,7 +257,7 @@ runDelayed Delayed{..} env req = do
         a <- authD
         b <- bodyD
         liftIO (writeIORef cleanupRef $ cleanupD b)
-        DelayedIO $ \ req -> return (serverD c a b req)
+        DelayedIO $ \ r -> return (serverD c a b r)
     )
     req
   fmap (routeRes,) $ readIORef cleanupRef
