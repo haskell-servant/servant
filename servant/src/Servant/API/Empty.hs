021bcd9e237d5a489bb953ea9724414f6c65a200
David Turner <dave.c.turner@gmail.com>
2017-05-16 16:03:25 +0000

    Document that EmptyAPI is, morally speaking, the unit of :<|>

--- servant/src/Servant/API/Empty.hs
+++ servant/src/Servant/API/Empty.hs
@@ -6,5 +6,7 @@ import           Data.Typeable    (Typeable)
 import           Prelude ()
 import           Prelude.Compat
 
--- | An empty API: one which serves nothing.
+-- | An empty API: one which serves nothing. Morally speaking, this should be
+-- the unit of ':<|>'. Implementors of interpretations of API types should
+-- treat 'EmptyAPI' as close to the unit as possible.
 data EmptyAPI = EmptyAPI deriving (Typeable, Eq, Show, Bounded)
