249a8386a5d60974cca941a69b7b81b6f30f7d24
David Turner <dave.c.turner@gmail.com>
2017-05-16 16:04:25 +0000

    Derive Enum on EmptyAPI

--- servant/src/Servant/API/Empty.hs
+++ servant/src/Servant/API/Empty.hs
@@ -9,4 +9,4 @@ import           Prelude.Compat
 -- | An empty API: one which serves nothing. Morally speaking, this should be
 -- the unit of ':<|>'. Implementors of interpretations of API types should
 -- treat 'EmptyAPI' as close to the unit as possible.
-data EmptyAPI = EmptyAPI deriving (Typeable, Eq, Show, Bounded)
+data EmptyAPI = EmptyAPI deriving (Typeable, Eq, Show, Bounded, Enum)
