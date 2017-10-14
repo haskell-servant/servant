5963761bacaf16c535934d04c6005f4e8d6aa294
Julian Arni <jkarni@gmail.com>
2017-10-14 07:53:04 -0700

    Unbias generation of requests. (haskell-servant/servant-quickcheck#19)
    * Unbias generation of requests:
    
         ...so that each endpoint is picked with the same frequency.
    
         Also, include a test for unbiased generation that measures mean and variance of results.

--- servant-quickcheck/test/Servant/QuickCheck/InternalSpec.hs
+++ servant-quickcheck/test/Servant/QuickCheck/InternalSpec.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE CPP #-}
 module Servant.QuickCheck.InternalSpec (spec) where
 
-import           Control.Concurrent.MVar (newMVar, readMVar, swapMVar)
-import           Control.Monad.IO.Class  (liftIO)
-import qualified Data.ByteString         as BS
-import qualified Data.ByteString.Char8   as C
+import           Control.Concurrent.MVar      (newMVar, readMVar, swapMVar)
+import           Control.Monad                (replicateM)
+import           Control.Monad.IO.Class       (liftIO)
+import qualified Data.ByteString              as BS
+import qualified Data.ByteString.Char8        as C
+import           Data.Maybe                   (fromJust)
 import           Prelude.Compat
 import           Servant
-import           Test.Hspec              (Spec, context, describe, it, shouldBe,
-                                          shouldContain)
-import           Test.Hspec.Core.Spec    (Arg, Example, Result (..),
-                                          defaultParams, evaluateExample)
-import           Test.QuickCheck.Gen     (unGen)
-import           Test.QuickCheck.Random  (mkQCGen)
-import           Network.HTTP.Client     (queryString, path)
+import           Test.Hspec                   (Spec, context, describe, it, shouldBe,
+                                               shouldContain)
+import           Test.Hspec.Core.Spec         (Arg, Example, Result (..),
+                                               defaultParams, evaluateExample)
+import           Test.QuickCheck.Gen          (unGen, generate)
+import           Test.QuickCheck.Random       (mkQCGen)
+import           Network.HTTP.Client          (queryString, path)
 
 #if MIN_VERSION_servant(0,8,0)
 import Servant.API.Internal.Test.ComprehensiveAPI (comprehensiveAPIWithoutRaw)
@@ -23,7 +25,8 @@ import Servant.API.Internal.Test.ComprehensiveAPI (ComprehensiveAPI,
 #endif
 
 import Servant.QuickCheck
-import Servant.QuickCheck.Internal (genRequest, serverDoesntSatisfy)
+import Servant.QuickCheck.Internal (genRequest, runGenRequest, serverDoesntSatisfy)
+
 
 spec :: Spec
 spec = do
@@ -35,6 +38,7 @@ spec = do
   queryParamsSpec
   queryFlagsSpec
   deepPathSpec
+  unbiasedGenerationSpec
 
 serversEqualSpec :: Spec
 serversEqualSpec = describe "serversEqual" $ do
@@ -45,8 +49,6 @@ serversEqualSpec = describe "serversEqual" $ do
         serversEqual api burl1 burl2 args bodyEquality
 
   context "when servers are not equal" $ do
-
-
     it "provides the failing responses in the error message" $ do
       Fail _ err <- withServantServer api2 server2 $ \burl1 ->
         withServantServer api2 server3 $ \burl2 -> do
@@ -76,10 +78,11 @@ serverSatisfiesSpec = describe "serverSatisfies" $ do
 
     it "fails with informative error messages" $ do
       Fail _ err <- withServantServerAndContext api ctx server $ \burl -> do
-        evalExample $ serverSatisfies api burl args (getsHaveCacheControlHeader <%> mempty)
-      err `shouldContain` "getsHaveCacheControlHeader"
+        evalExample $ serverSatisfies api burl args (notAllowedContainsAllowHeader <%> mempty)
+      err `shouldContain` "notAllowedContainsAllowHeader"
       err `shouldContain` "Headers"
       err `shouldContain` "Body"
+
 
 onlyJsonObjectSpec :: Spec
 onlyJsonObjectSpec = describe "onlyJsonObjects" $ do
@@ -120,7 +123,7 @@ deepPathSpec = describe "Path components" $ do
   it "are separated by slashes, without a trailing slash" $ do
     let rng = mkQCGen 0
         burl = BaseUrl Http "localhost" 80 ""
-        gen = genRequest deepAPI
+        gen = runGenRequest deepAPI
         req = (unGen gen rng 0) burl
     path req `shouldBe` ("/one/two/three")
 
@@ -131,7 +134,7 @@ queryParamsSpec = describe "QueryParams" $ do
   it "reduce to an HTTP query string correctly" $ do
     let rng = mkQCGen 0
         burl = BaseUrl Http "localhost" 80 ""
-        gen = genRequest paramsAPI
+        gen = runGenRequest paramsAPI
         req = (unGen gen rng 0) burl
         qs = C.unpack $ queryString req
     qs `shouldBe` "one=_&two=_"
@@ -142,10 +145,32 @@ queryFlagsSpec = describe "QueryFlags" $ do
   it "reduce to an HTTP query string correctly" $ do
     let rng = mkQCGen 0
         burl = BaseUrl Http "localhost" 80 ""
-        gen = genRequest flagsAPI
+        gen = runGenRequest flagsAPI
         req = (unGen gen rng 0) burl
         qs = C.unpack $ queryString req
     qs `shouldBe` "one&two"
+
+makeRandomRequest :: Proxy LargeAPI -> BaseUrl -> IO Integer
+makeRandomRequest large burl = do
+  req <- generate $ runGenRequest large
+  pure $ fst . fromJust . C.readInteger . C.drop 1 . path $ req burl
+
+
+unbiasedGenerationSpec :: Spec
+unbiasedGenerationSpec = describe "Unbiased Generation of requests" $
+
+  it "frequency paired with generated endpoint should be more randomly distributed" $ do
+    let burl = BaseUrl Http "localhost" 80 ""
+    let runs = 10000 :: Double
+    someRequests <- replicateM 10000 (makeRandomRequest largeApi burl)
+    let mean = (sum $ map fromIntegral someRequests) / runs
+    let variancer x = let ix = fromIntegral x in (ix - mean) * (ix - mean)
+    let variance = (sum $ map variancer someRequests) / runs - 1
+    -- mean should be around 8
+    mean > 7 `shouldBe` True
+    mean < 9 `shouldBe` True
+    -- Std dev is likely around 4. Variance is probably greater than 20.
+    variance > 19.5 `shouldBe` True
 
 ------------------------------------------------------------------------------
 -- APIs
@@ -193,6 +218,29 @@ server2 = return $ return 1
 
 server3 :: IO (Server API2)
 server3 = return $ return 2
+
+
+largeApi :: Proxy LargeAPI
+largeApi = Proxy
+
+type LargeAPI
+  =    "1" :> Get '[JSON] Int
+  :<|> "2" :> Get '[JSON] Int
+  :<|> "3" :> Get '[JSON] Int
+  :<|> "4" :> Get '[JSON] Int
+  :<|> "5" :> Get '[JSON] Int
+  :<|> "6" :> Get '[JSON] Int
+  :<|> "7" :> Get '[JSON] Int
+  :<|> "8" :> Get '[JSON] Int
+  :<|> "9" :> Get '[JSON] Int
+  :<|> "10" :> Get '[JSON] Int
+  :<|> "11" :> Get '[JSON] Int
+  :<|> "12" :> Get '[JSON] Int
+  :<|> "13" :> Get '[JSON] Int
+  :<|> "14" :> Get '[JSON] Int
+  :<|> "15" :> Get '[JSON] Int
+  :<|> "16" :> Get '[JSON] Int
+
 
 type OctetAPI = Get '[OctetStream] BS.ByteString
 
