d64a6625265ce801265f45a271393c9b967adcfd
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-07-06 11:45:41 +0300

    Update doc deps

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='servant-rtd',
+      version='0.0.1',
+      description='Documentation for the haskell-servant platform',
+      url='https://github.com/haskell-servant/servant',
+      author='Servant Contributors',
+      author_email='haskell-servant-maintainers@googlegroups.com',
+      license='BSD3',
+      zip_safe=False)
+
