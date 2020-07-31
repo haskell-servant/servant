19018405a79d5a40c8d12546a47262d486e21020
Matthias Fischmann <mf@zerobuzz.net>
2020-07-31 15:45:11 +0200

    Improve types & haddocks of uverb client helpers.

--- servant-client-core/src/Servant/Client/Core/HasClient.hs
+++ servant-client-core/src/Servant/Client/Core/HasClient.hs
@@ -85,6 +85,8 @@ import           Servant.API.Modifiers
                  (FoldRequired, RequiredArgument, foldRequiredArgument)
 import           Servant.API.UVerb
                  (HasStatus, Statuses, UVerb, Union, Unique, inject, statusOf)
+import           Servant.API.UVerb.OpenUnion
+                 (IsMember)
 
 import           Servant.Client.Core.Auth
 import           Servant.Client.Core.BasicAuth
@@ -316,18 +318,26 @@ instance {-# OVERLAPPING #-}
 
   hoistClientMonad _ _ f ma = f ma
 
--- | convenience function to extract an unknown union element using a type class.
+-- | Convenience function to apply a function to an unknown union element using a type class.
+-- All elements of the union must have instances in the type class, and the function is
+-- applied unconditionally.
+--
+-- See also: 'extractUResp'.
 collapseUResp ::
-  forall c a as.
+  forall (c :: * -> Constraint) (a :: *) (as :: [*]).
   All c as =>
-  Proxy (c :: * -> Constraint) ->
-  (forall x. c x => x -> a) ->
-  Union as ->
-  a
+  Proxy c -> (forall x. c x => x -> a) -> Union as -> a
 collapseUResp proxy render = collapse_NS . cmap_NS proxy (K . render . runIdentity)
 
--- | convenience function to extract an unknown union element using 'cast'.
-extractUResp :: forall a as. (All Typeable as, Typeable a) => Union as -> Maybe a
+-- | Convenience function to extract a union element using 'cast', ie. return the value if the
+-- selected type happens to be the actual type of the union in this value, or 'Nothing'
+-- otherwise.
+--
+-- See also: 'collapseUResp'.
+extractUResp ::
+  forall (a :: *) (as :: [*]).
+  (All Typeable as, Typeable a, IsMember a as) =>
+  Union as -> Maybe a
 extractUResp = collapse_NS . cmap_NS (Proxy @Typeable) (K . cast . runIdentity)
 
 data ClientParseError = ClientParseError MediaType String | ClientStatusMismatch | ClientNoMatchingStatus
