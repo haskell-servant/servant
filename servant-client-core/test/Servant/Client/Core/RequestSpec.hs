1d53abcd0be07f90862bc65608fe50c149ba81a6
Antoine Vandecreme <antoine.vandecreme@fretlink.com>
2021-12-01 16:34:10 +0100

    Fix Request's Show instance

--- servant-client-core/test/Servant/Client/Core/RequestSpec.hs
+++ servant-client-core/test/Servant/Client/Core/RequestSpec.hs
@@ -10,10 +10,22 @@ import           Data.List (isInfixOf)
 import           Servant.Client.Core.Request
 import           Test.Hspec
 
+newtype DataWithRequest = DataWithRequest (RequestF RequestBody ())
+  deriving Show
+
 spec :: Spec
 spec = do
   describe "Request" $ do
     describe "show" $ do
+      it "has parenthesis correctly positioned" $ do
+        let d = DataWithRequest (void defaultRequest)
+        show d `shouldBe` "DataWithRequest (Request {requestPath = ()\
+                                                  \, requestQueryString = fromList []\
+                                                  \, requestBody = Nothing\
+                                                  \, requestAccept = fromList []\
+                                                  \, requestHeaders = fromList []\
+                                                  \, requestHttpVersion = HTTP/1.1\
+                                                  \, requestMethod = \"GET\"})"
       it "redacts the authorization header" $ do
         let request = void $ defaultRequest { requestHeaders = pure ("authorization", "secret") }
         isInfixOf "secret" (show request) `shouldBe` False
