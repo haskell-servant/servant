30cf13c50c6b1915bdf9b228de3865c57c01f804
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-05-14 22:16:46 +0300

    improve and move example

--- doc/cookbook/servant-queryparam-example/Main.hs
+++ doc/cookbook/servant-queryparam-example/Main.hs
@@ -7,34 +7,33 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+import Control.Monad.IO.Class (MonadIO (..))
+import Control.Monad.Trans.Except (ExceptT (..))
 import Data.Aeson (ToJSON)
 import qualified Data.ByteString.Lazy.Char8 as BSL8
 import Data.Data (Proxy (..), Typeable)
+import Data.Maybe (maybeToList)
 import Data.OpenApi (OpenApi, ToParamSchema, ToSchema)
 import Data.OpenApi.Internal.Utils (encodePretty)
 import GHC.Generics (Generic)
 import GHC.TypeLits (Symbol)
-import Servant (GenericMode ((:-)), Get, JSON, NamedRoutes, (:>))
-import Servant.Named ()
+import qualified Network.Wai.Handler.Warp as Warp
+import Servant (Application, Context (..), GenericMode ((:-)), Get, Handler (..), JSON, NamedRoutes, ServerError, (:>))
 import Servant.OpenApi (HasOpenApi (..))
-import Servant.Record (RecordParam, UnRecordParam)
-import Servant.Server.Named ()
+import Servant.OpenApi.Record ()
+import Servant.Record (RecordParam)
+import Servant.Server.Generic (genericServe, genericServeTWithContext)
 import Servant.Server.Record ()
 import Servant.Symbols (DropPrefix, Eval, Exp)
-
--- | Instance of 'HasOpenAPI' for any 'RecordParam'
-instance HasOpenApi (UnRecordParam mod (RecordParam mod a :> api)) => HasOpenApi (RecordParam mod a :> api) where
-  toOpenApi :: Proxy (RecordParam mod a :> api) -> OpenApi
-  toOpenApi _ = toOpenApi (Proxy :: Proxy (UnRecordParam mod (RecordParam mod a :> api)))
 
 -- | A label for dropping the prefix of a symbol
 data DropPrefixExp :: Symbol -> Exp Symbol
@@ -47,22 +46,22 @@ data KeepPrefixExp :: Symbol -> Exp Symbol
 type instance Eval (KeepPrefixExp sym) = sym
 
 -- | Query parameters as a record
-newtype Params = Params {_get_user :: Maybe String} deriving (Show, Generic, Typeable, ToJSON, ToSchema)
+data Params = Params {_get_user :: Maybe String, _get_users :: [String], _get_oneUser :: String, _get_userFlag :: Bool} deriving (Show, Generic, Typeable, ToJSON, ToSchema)
 
 -- | User id
 newtype UserId = UserId Integer deriving (Show, Generic, Typeable, ToJSON, ToSchema, ToParamSchema)
 
 -- | API as a record. Prefixes of query parameters are dropped
-newtype GetUser1 routes = GetUser1 {get :: routes :- "get" :> RecordParam DropPrefixExp Params :> Get '[JSON] Int} deriving (Generic)
+newtype UserAPI1 routes = UserAPI1 {get :: routes :- "get" :> RecordParam DropPrefixExp Params :> Get '[JSON] [String]} deriving (Generic)
 
 -- | API as a type synonym
-type APIDrop = NamedRoutes GetUser1
+type APIDrop = NamedRoutes UserAPI1
 
 -- | API as a record. Prefixes of query parameters are kept
-newtype GetUser2 routes = GetUser2 {get :: routes :- "get" :> RecordParam KeepPrefixExp Params :> Get '[JSON] Int} deriving (Generic)
+newtype UserAPI2 routes = UserAPI2 {get :: routes :- "get" :> RecordParam KeepPrefixExp Params :> Get '[JSON] [String]} deriving (Generic)
 
 -- | API as a type synonym
-type APIKeep = NamedRoutes GetUser2
+type APIKeep = NamedRoutes UserAPI2
 
 -- | 'OpenApi' specification for 'APIDrop'
 specDrop :: OpenApi
@@ -72,9 +71,23 @@ specDrop = toOpenApi (Proxy :: Proxy APIDrop)
 specKeep :: OpenApi
 specKeep = toOpenApi (Proxy :: Proxy APIKeep)
 
+server :: Application
+server =
+  genericServeTWithContext
+    (\x -> Servant.Handler (ExceptT $ Right <$> liftIO x))
+    UserAPI1
+      { get = \Params{..} -> pure $ maybeToList _get_user <> _get_users <> [_get_oneUser] <> [if _get_userFlag then "true" else "false"]
+      }
+    EmptyContext
+
 main :: IO ()
 main = do
   putStrLn "\n---\nQuery parameters without prefixes\n---\n"
   BSL8.putStrLn $ encodePretty specDrop
   putStrLn "\n---\nQuery parameters with prefixes\n---\n"
   BSL8.putStrLn $ encodePretty specKeep
+
+  putStrLn "Starting the server..."
+  putStrLn "\nTry running\n"
+  putStrLn "curl -v \"localhost:8080/get?user=1&users=2&users=3&oneUser=4\""
+  Warp.run 8080 server
