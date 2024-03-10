86bf6dc04a17c2161dfa99c664c48951175615d2
mangoiv <contact@mangoiv.com>
2024-03-10 14:27:58 +0100

    [feat] nix

---
+++ servant-quickcheck/flake.nix
@@ -0,0 +1,46 @@
+{
+  nixConfig.allow-import-from-derivation = true;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = inputs:
+    inputs.parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [
+        inputs.haskell-flake.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+      ];
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = {
+            cabal-fmt.enable = true;
+            stylish-haskell.enable = true;
+
+            alejandra.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+          };
+        };
+        haskellProjects.default = {
+          packages = {};
+          settings = {};
+          devShell.mkShellArgs.shellHook = config.pre-commit.installationScript;
+        };
+        haskellProjects.ghc96 = {
+          packages = {};
+          settings = {};
+          devShell.mkShellArgs.shellHook = config.pre-commit.installationScript;
+          basePackages = pkgs.haskell.packages.ghc96;
+        };
+      };
+    };
+}
