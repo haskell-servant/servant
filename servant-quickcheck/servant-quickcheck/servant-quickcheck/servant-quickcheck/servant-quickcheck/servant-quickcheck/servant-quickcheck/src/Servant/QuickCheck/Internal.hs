ff1875d44407830345a1aec9a516475ffae672cc
Théophile Choutri <theophile@choutri.eu>
2024-04-22 10:28:44 +0200

    Remove extraneous source dir

--- servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/src/Servant/QuickCheck/Internal.hs
+++
@@ -1,6 +0,0 @@
-module Servant.QuickCheck.Internal (module X) where
-
-import Servant.QuickCheck.Internal.Testable as X
-import Servant.QuickCheck.Internal.Predicates as X
-import Servant.QuickCheck.Internal.QuickCheck as X
-import Servant.QuickCheck.Internal.Benchmarking as X
