9ae87c9bce52e43fe0bc8fcb7d9ec4e01c44e133
Daniel Casanueva <daniel.casanueva.git@proton.me>
2025-01-11 12:45:01 +0100

    Improve documentation

--- servant/src/Servant/API/Host.hs
+++ servant/src/Servant/API/Host.hs
@@ -5,7 +5,9 @@ import GHC.TypeLits (Symbol)
 
 -- | Match against the given host.
 --
--- Example:
+--   This allows you to define APIs over multiple domains. For example:
 --
--- > type MyApi = Host "example.com" :> PostNoContent
+-- > type API = Host "api1.example" :> API1
+-- >       :<|> Host "api2.example" :> API2
+--
 data Host (sym :: Symbol) deriving Typeable
