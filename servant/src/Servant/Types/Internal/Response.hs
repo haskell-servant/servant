fc3e9a09aed754dd9a10c28abafa7f8567167c81
Théophile Choutri <theophile@choutri.eu>
2025-01-16 21:22:30 +0100

    Rename Servant.Types.Internal.ResponseList to Servant.Types.Internal.ResponseList

--- servant/src/Servant/Types/Internal/Response.hs
+++ servant/src/Servant/Types/Internal/Response.hs
@@ -3,7 +3,7 @@
 -- | This module offers other servant libraries a minimalistic HTTP response type.
 --
 -- It is purely an internal API and SHOULD NOT be used by end-users of Servant.
-module Servant.Types.Internal.ResponseList where
+module Servant.Types.Internal.Response where
 
 import Network.HTTP.Types (Status, Header)
 import Data.Sequence (Seq)
