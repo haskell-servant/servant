f18ff26b1a625f417134c527c4ad3251af66a6c1
Théophile Choutri <theophile@choutri.eu>
2025-06-23 15:24:37 +0200

    Add changelog entry

--- servant-server/src/Servant/Server/Internal/DelayedIO.hs
+++ servant-server/src/Servant/Server/Internal/DelayedIO.hs
@@ -5,7 +5,7 @@
 module Servant.Server.Internal.DelayedIO where
 
 import Control.Monad.Base (MonadBase (..))
-import Control.Monad.Catch (MonadThrow (..), MonadCatch(..), MonadMask)
+import Control.Monad.Catch (MonadCatch (..), MonadMask, MonadThrow (..))
 import Control.Monad.Reader (MonadReader (..), ReaderT (..), runReaderT)
 import Control.Monad.Trans (MonadIO (..), MonadTrans (..))
 import Control.Monad.Trans.Control (MonadBaseControl (..))
@@ -30,12 +30,12 @@ newtype DelayedIO a = DelayedIO {runDelayedIO' :: ReaderT Request (ResourceT (Ro
     ( Applicative
     , Functor
     , Monad
+    , MonadCatch
     , MonadIO
+    , MonadMask
     , MonadReader Request
     , MonadResource
     , MonadThrow
-    , MonadCatch
-    , MonadMask
     )
 
 instance MonadBase IO DelayedIO where
@@ -54,7 +54,6 @@ instance MonadBaseControl IO DelayedIO where
     liftBaseWith $ \runInBase -> f $ \x ->
       runInBase (runInternalState (runReaderT (runDelayedIO' x) req) s)
   restoreM = DelayedIO . lift . withInternalState . const . restoreM
-
 
 runDelayedIO :: DelayedIO a -> Request -> ResourceT IO (RouteResult a)
 runDelayedIO m req = transResourceT runRouteResultT $ runReaderT (runDelayedIO' m) req
