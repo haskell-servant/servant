55ca12a56c7cb070475b2e38d9491dcead9c0a7b
Laurent <laurent.renedecotret@kraken.com>
2026-07-30 11:58:08 -0400

    Contribute servant-openapi3

---
+++ servant-openapi3/example/test/TodoSpec.hs
@@ -0,0 +1,29 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module TodoSpec where
+
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BL8
+import Prelude.Compat
+import Servant.OpenApi.Test
+import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
+import Prelude ()
+
+import Paths_example
+import Todo
+
+spec :: Spec
+spec = describe "Swagger" $ do
+  context "ToJSON matches ToSchema" $ validateEveryToJSON todoAPI
+  it "swagger.json is up-to-date" $ do
+    path <- getDataFileName "swagger.json"
+    swag <- eitherDecode <$> BL8.readFile path
+    swag `shouldBe` Right todoSwagger
+
+instance Arbitrary Todo where
+  arbitrary = Todo <$> arbitrary <*> arbitrary
+
+instance Arbitrary TodoId where
+  arbitrary = TodoId <$> arbitrary
---
+++ servant-openapi3/src/Servant/OpenApi/Internal/TypeLevel.hs
@@ -0,0 +1,9 @@
+module Servant.OpenApi.Internal.TypeLevel
+  ( module Servant.OpenApi.Internal.TypeLevel.API
+  , module Servant.OpenApi.Internal.TypeLevel.Every
+  , module Servant.OpenApi.Internal.TypeLevel.TMap
+  ) where
+
+import Servant.OpenApi.Internal.TypeLevel.API
+import Servant.OpenApi.Internal.TypeLevel.Every
+import Servant.OpenApi.Internal.TypeLevel.TMap
